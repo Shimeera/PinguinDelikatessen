283c57ff38622da593d540e0dfed9a5c0c5b7cd7
unknown <jglath@gmx.de>
2026-03-03 11:44:36 +0100

    Header: Überschrift  und Logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProduktListeComponent } from './produkt-liste/produkt-liste';
+import { ProduktListeComponent } from './produkt-liste/produkt-liste'; 
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ProduktListeComponent],
+  imports: [RouterOutlet,Header,ProduktListeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
